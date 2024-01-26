f7865b7aafb259e2d1ce643a19caf51926446a90
Subhayu <s7chak@gmail.com>
2024-01-26 14:38:37 -0700

    Cloud run flask app: porting

--- app.py
+++ app.py
@@ -51,6 +51,5 @@ def home():
     print('Reached API home')
     return jsonify({'API':"Topicverse"})
 
-# if __name__ == '__main__':
-port = int(os.environ.get('PORT', 8080))
-app.run(host="0.0.0.0", port=port)
+if __name__ == '__main__':
+	app.run(host="0.0.0.0")
