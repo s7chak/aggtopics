7c5972134e8cc9013f447e2e8ad3b9719dceb133
Subhayu <s7chak@gmail.com>
2024-01-28 13:47:23 -0700

    save multi topics

--- app.py
+++ app.py
@@ -29,19 +29,25 @@ with open(json_file_path, 'r') as json_file:
     sources = json.load(json_file)
 
 
+
 @app.route('/fetchstory')
 def fetch_story():
     print(request.args)
     story_type = request.args.get('type').lower()
     print("Starting daily story fetch...", story_type)
     start_time = time.time()
-    soups = ops.fetch_article_soups(sources, [story_type])
-    exc_list = exc_map[story_type.lower()]
-    data = ops.process_article_soups(soups, exc_list)
-    ops.do_text_preprocessing(data)
-    data.to_csv(today_date + '.csv', index=False)
-    filepath = story_type.lower()+'/'+today_date + '.csv'
-    ops.upload_blob(filepath, bucket_name)
+    try:
+        soups = ops.fetch_article_soups(sources, [story_type])
+        exc_list = exc_map[story_type]
+        data = ops.process_article_soups(soups, exc_list)
+        ops.do_text_preprocessing(data)
+        data.to_csv(today_date + '.csv', index=False)
+        filepath = story_type.lower()+'/'+today_date + '.csv'
+        ops.upload_blob(filepath, bucket_name)
+        # ops.save_doc(data, filepath) # local
+        os.remove(today_date + '.csv')
+    except:
+        return jsonify({'API': "Topicverse", 'call': "fetchstory:" + story_type, "status": 'Failure'})
     end_time = time.time()
     elapsed_time = end_time - start_time
     total_elapsed_time = end_time - func_start_time
@@ -49,6 +55,7 @@ def fetch_story():
     print(f"Run Time: {total_elapsed_time} seconds")
     print(f"Story fetch ended for type:",story_type)
     return jsonify({'API':"Topicverse", 'call': "fetchstory:"+story_type, "status": 'Complete'})
+
 
 
 @app.route('/')
