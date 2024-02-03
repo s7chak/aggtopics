09b7265ab945956175702cfedc0ac3f93593656a
s7chak <s7chak@gmail.com>
2024-02-02 19:16:14 -0700

    .

--- ops.py
+++ ops.py
@@ -7,25 +7,22 @@ import nltk
 import os
 import pandas as pd
 import pandas as pd
-import pyLDAvis
+
 import re
 import requests
-import requests
 import time
-import time
-from bs4 import BeautifulSoup
 from bs4 import BeautifulSoup
 from collections import Counter
 from datetime import datetime
-from datetime import datetime
 from flask import Flask, request
-from gensim.corpora import Dictionary
-from gensim.models import LdaModel, CoherenceModel
+# from pyLDAvis import gensim
+# from gensim.corpora import Dictionary
+# from gensim.models import LdaModel, CoherenceModel
+# import pyLDAvis
 from io import BytesIO
 from nltk import word_tokenize, WordNetLemmatizer
 from nltk.corpus import stopwords
 from pathlib import Path
-from pyLDAvis import gensim
 from wordcloud import WordCloud
 from google.cloud import storage
 import ops
@@ -190,124 +187,124 @@ def do_wordclouds(data, fields):
     return res
 
 
-def do_lda_html(data, field):
-    field = 'Processed_Text'
-    processed_titles = data[field].apply(eval)
-    dictionary = Dictionary(processed_titles)
-    corpus = [dictionary.doc2bow(title) for title in processed_titles]
-    coherence_values = []
-    model_list = []
-    for num_topics in range(1, round(len(processed_titles)/5)):
-        lda_model = LdaModel(corpus, num_topics=num_topics, id2word=dictionary)
-        model_list.append(lda_model)
-        coherencemodel = CoherenceModel(model=lda_model, texts=data[field].apply(eval).to_list(), dictionary=dictionary, coherence='c_v')
-        coherence_values.append(coherencemodel.get_coherence())
+# def do_lda_html(data, field):
+#     field = 'Processed_Text'
+#     processed_titles = data[field].apply(eval)
+#     dictionary = Dictionary(processed_titles)
+#     corpus = [dictionary.doc2bow(title) for title in processed_titles]
+#     coherence_values = []
+#     model_list = []
+#     for num_topics in range(1, round(len(processed_titles)/5)):
+#         lda_model = LdaModel(corpus, num_topics=num_topics, id2word=dictionary)
+#         model_list.append(lda_model)
+#         coherencemodel = CoherenceModel(model=lda_model, texts=data[field].apply(eval).to_list(), dictionary=dictionary, coherence='c_v')
+#         coherence_values.append(coherencemodel.get_coherence())
 
-    optimal_num_topics = coherence_values.index(max(coherence_values)) + 1
-    optimal_lda_model = LdaModel(corpus, num_topics=optimal_num_topics, id2word=dictionary)
+#     optimal_num_topics = coherence_values.index(max(coherence_values)) + 1
+#     optimal_lda_model = LdaModel(corpus, num_topics=optimal_num_topics, id2word=dictionary)
 
-    print(f"Optimal Number of Topics: {optimal_num_topics}")
-    for topic_num in range(optimal_num_topics):
-        print(f"Topic {topic_num + 1}: {optimal_lda_model.print_topic(topic_num)}")
+#     print(f"Optimal Number of Topics: {optimal_num_topics}")
+#     for topic_num in range(optimal_num_topics):
+#         print(f"Topic {topic_num + 1}: {optimal_lda_model.print_topic(topic_num)}")
 
-    if optimal_num_topics > 1:
-        prepared_data = pyLDAvis.gensim.prepare(optimal_lda_model, corpus, dictionary)
-        html_string = pyLDAvis.prepared_data_to_html(prepared_data)
-        html_path = Path("output/lda_viz.html")
-        pyLDAvis.save_html(prepared_data, str(html_path))
-        return html_string
-    return None
+#     if optimal_num_topics > 1:
+#         prepared_data = pyLDAvis.gensim.prepare(optimal_lda_model, corpus, dictionary)
+#         html_string = pyLDAvis.prepared_data_to_html(prepared_data)
+#         html_path = Path("output/lda_viz.html")
+#         pyLDAvis.save_html(prepared_data, str(html_path))
+#         return html_string
+#     return None
 
 
-def do_dmm_analysis(dictionary, texts):
-    group_topics = 10
-    gsdmm = MovieGroupProcess(K=group_topics, alpha=0.1, beta=0.3, n_iters=group_topics)
-    y = gsdmm.fit(texts, len(dictionary))
+# def do_dmm_analysis(dictionary, texts):
+#     group_topics = 10
+#     gsdmm = MovieGroupProcess(K=group_topics, alpha=0.1, beta=0.3, n_iters=group_topics)
+#     y = gsdmm.fit(texts, len(dictionary))
 
-    doc_count = np.array(gsdmm.cluster_doc_count)
-    print('Number of documents per topic :', doc_count)
+#     doc_count = np.array(gsdmm.cluster_doc_count)
+#     print('Number of documents per topic :', doc_count)
 
-    # Topics sorted by the number of document they are allocated to
-    top_index = doc_count.argsort()[-group_topics:][::-1]
-    print('Most important clusters (by number of docs inside):', top_index)
+#     # Topics sorted by the number of document they are allocated to
+#     top_index = doc_count.argsort()[-group_topics:][::-1]
+#     print('Most important clusters (by number of docs inside):', top_index)
 
-    # define function to get top words per topic
-    def top_words(cluster_word_distribution, top_cluster, values):
-        for cluster in top_cluster:
-            sort_dicts = sorted(cluster_word_distribution[cluster].items(), key=lambda k: k[1], reverse=True)[:values]
-            print("\nCluster %s : %s" % (cluster, sort_dicts))
+#     # define function to get top words per topic
+#     def top_words(cluster_word_distribution, top_cluster, values):
+#         for cluster in top_cluster:
+#             sort_dicts = sorted(cluster_word_distribution[cluster].items(), key=lambda k: k[1], reverse=True)[:values]
+#             print("\nCluster %s : %s" % (cluster, sort_dicts))
 
-    # get top words in topics
-    top_words(gsdmm.cluster_word_distribution, top_index, 20)
+#     # get top words in topics
+#     top_words(gsdmm.cluster_word_distribution, top_index, 20)
 
-    cluster_word_distribution = gsdmm.cluster_word_distribution
+#     cluster_word_distribution = gsdmm.cluster_word_distribution
 
-    topic_num = 0
-    # Select topic you want to output as dictionary (using topic_number)
-    topic_dict = sorted(cluster_word_distribution[topic_num].items(), key=lambda k: k[1], reverse=True)  # [:values]
+#     topic_num = 0
+#     # Select topic you want to output as dictionary (using topic_number)
+#     topic_dict = sorted(cluster_word_distribution[topic_num].items(), key=lambda k: k[1], reverse=True)  # [:values]
 
-    # Generate a word cloud image
-    wordcloud = WordCloud(background_color='#fcf2ed',
-                          width=1000,
-                          height=600,
-                          colormap='flag').generate_from_frequencies(topic_dict)
+#     # Generate a word cloud image
+#     wordcloud = WordCloud(background_color='#fcf2ed',
+#                           width=1000,
+#                           height=600,
+#                           colormap='flag').generate_from_frequencies(topic_dict)
 
-    # Print to screen
-    fig, ax = plt.subplots(figsize=[20, 10])
-    plt.imshow(wordcloud, interpolation='bilinear')
-    plt.axis("off");
-    # Save to disk
-    plt.savefig('dmm_summary_cloud.png')
+#     # Print to screen
+#     fig, ax = plt.subplots(figsize=[20, 10])
+#     plt.imshow(wordcloud, interpolation='bilinear')
+#     plt.axis("off");
+#     # Save to disk
+#     plt.savefig('dmm_summary_cloud.png')
 
-def do_topicwizard_analysis(dictionary, texts):
-    min_topics = 1
+# def do_topicwizard_analysis(dictionary, texts):
+#     min_topics = 1
 
-    vectorizer = CountVectorizer(min_df=min_topics, max_df=5)
+#     vectorizer = CountVectorizer(min_df=min_topics, max_df=5)
 
-    # Creating a Dirichlet Multinomial Mixture Model with 30 components
-    dmm = DMM(n_components=5, n_iterations=100, alpha=0.1, beta=0.1)
+#     # Creating a Dirichlet Multinomial Mixture Model with 30 components
+#     dmm = DMM(n_components=5, n_iterations=100, alpha=0.1, beta=0.1)
 
-    # Creating topic pipeline
-    pipeline = Pipeline([
-        ("vectorizer", vectorizer),
-        ("dmm", dmm),
-    ])
-    full_string = texts[0]
-    pipeline.fit(full_string)
-    topicwizard.visualize(pipeline=pipeline, corpus=full_string)
+#     # Creating topic pipeline
+#     pipeline = Pipeline([
+#         ("vectorizer", vectorizer),
+#         ("dmm", dmm),
+#     ])
+#     full_string = texts[0]
+#     pipeline.fit(full_string)
+#     topicwizard.visualize(pipeline=pipeline, corpus=full_string)
 
-def do_lda_analysis(df, corpus, dictionary, texts):
-    coherence_values = []
-    model_list = []
-    for num_topics in range(2, 4):
-        lda_model = models.LdaModel(corpus, num_topics=num_topics, id2word=dictionary)
-        model_list.append(lda_model)
-        coherencemodel = CoherenceModel(model=lda_model, texts=df['Processed_Text'], dictionary=dictionary, coherence='c_v')
-        coherence_values.append(coherencemodel.get_coherence())
-    end_time = time.time()
-    elapsed_time = end_time - start_time
+# def do_lda_analysis(df, corpus, dictionary, texts):
+#     coherence_values = []
+#     model_list = []
+#     for num_topics in range(2, 4):
+#         lda_model = models.LdaModel(corpus, num_topics=num_topics, id2word=dictionary)
+#         model_list.append(lda_model)
+#         coherencemodel = CoherenceModel(model=lda_model, texts=df['Processed_Text'], dictionary=dictionary, coherence='c_v')
+#         coherence_values.append(coherencemodel.get_coherence())
+#     end_time = time.time()
+#     elapsed_time = end_time - start_time
 
-    print(f"LDA Time: {elapsed_time} seconds")
+#     print(f"LDA Time: {elapsed_time} seconds")
 
-    optimal_num_topics = coherence_values.index(max(coherence_values)) + 2  # Adding 2 because we started the loop from 2
-    optimal_lda_model = models.LdaModel(corpus, num_topics=optimal_num_topics, id2word=dictionary)
+#     optimal_num_topics = coherence_values.index(max(coherence_values)) + 2  # Adding 2 because we started the loop from 2
+#     optimal_lda_model = models.LdaModel(corpus, num_topics=optimal_num_topics, id2word=dictionary)
 
-    print(f"Optimal Number of Topics: {optimal_num_topics}")
-    for topic_num in range(optimal_num_topics):
-        print(f"Topic {topic_num + 1}: {optimal_lda_model.print_topic(topic_num)}")
+#     print(f"Optimal Number of Topics: {optimal_num_topics}")
+#     for topic_num in range(optimal_num_topics):
+#         print(f"Topic {topic_num + 1}: {optimal_lda_model.print_topic(topic_num)}")
 
-    # topic_assignments = [max(lda_model[doc], key=lambda x: x[1])[0] for doc in corpus]
-    # # Topic Summarization
-    # topic_summaries = [lda_model.print_topic(topic_num) for topic_num in range(lda_model.num_topics)]
-    #
-    # # Quality Evaluation
-    # coherence_model = CoherenceModel(model=lda_model, texts=texts, dictionary=dictionary, coherence='c_v')
-    # coherence_score = coherence_model.get_coherence()
-    # perplexity_score = lda_model.log_perplexity(corpus)
+#     # topic_assignments = [max(lda_model[doc], key=lambda x: x[1])[0] for doc in corpus]
+#     # # Topic Summarization
+#     # topic_summaries = [lda_model.print_topic(topic_num) for topic_num in range(lda_model.num_topics)]
+#     #
+#     # # Quality Evaluation
+#     # coherence_model = CoherenceModel(model=lda_model, texts=texts, dictionary=dictionary, coherence='c_v')
+#     # coherence_score = coherence_model.get_coherence()
+#     # perplexity_score = lda_model.log_perplexity(corpus)
 
-    prepared_data = pyLDAvis.gensim.prepare(optimal_lda_model, corpus, dictionary)
-    # pyLDAvis.display(prepared_data)
-    # pyLDAvis.save_html(prepared_data, image_path+'topic_cluster.html')
-    html_content = pyLDAvis.prepared_data_to_html(prepared_data)
-    report_collection['data']['pyldavis_html'] = html_content
+#     prepared_data = pyLDAvis.gensim.prepare(optimal_lda_model, corpus, dictionary)
+#     # pyLDAvis.display(prepared_data)
+#     # pyLDAvis.save_html(prepared_data, image_path+'topic_cluster.html')
+#     html_content = pyLDAvis.prepared_data_to_html(prepared_data)
+#     report_collection['data']['pyldavis_html'] = html_content
 
